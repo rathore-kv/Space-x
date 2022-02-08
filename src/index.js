1c97e7a4bccd45274dc0ed4aa3873e962645fd53
rathore-kv <98039759+rathore-kv@users.noreply.github.com>
2022-02-08 10:33:38 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+serviceWorker.register();
+serviceWorkerRegistration.register();
